93fd2eda181c12d76a54ee66e9087704504c07a9
Kehinde Yusuf <mubaraq>
2024-08-26 12:27:50 +0100

    Resolved conflicting merges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
+import LandingPage from './Pages/LandingPage'
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import UrlProvider from './context';
+
+import AppLayout from "./layouts/app-layout";
+import RequireAuth from './components/ui/require-auth';
+
+import RedirectLink from './Pages/Redirect-link';
+import Dashboard from './Pages/Dashboard';
+import Link from './Pages/Link';
+import Auth from "./Pages/auth";
 import './App.css'
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
+
 
 function App() {
 
+
   return (
     <>
-      <h1>hello world</h1>
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
     </>
   )
 }
