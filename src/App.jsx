781601c6d159220f7bdfe3600b9aa1daae295b76
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2024-08-27 19:48:08 +0100

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,74 @@
-// import React, { Suspense } from 'react';
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import AppLayout from "./layouts/app-layout";
-// import RequireAuth from './components/ui/require-auth';
-import './App.css';
+import React, { Suspense } from 'react';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import RequireAuth from './components/ui/require-auth';
+import UrlProvider from "./context";
+import './index.css';
 
-// Lazy load the components
-// const LandingPage = React.lazy(() => import('./Pages/LandingPage'));
-// const RedirectLink = React.lazy(() => import('./Pages/Redirect-link'));
-// const Dashboard = React.lazy(() => import('./Pages/Dashboard'));
-// const Link = React.lazy(() => import('./Pages/Link'));
-// const Auth = React.lazy(() => import('./Pages/auth'));
 
-// const router = createBrowserRouter([
-//   {
-//     element: <AppLayout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: (
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <LandingPage />
-//           </Suspense>
-//         ),
-//       },
-//       {
-//         path: "/auth",
-//         element: (
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <Auth />
-//           </Suspense>
-//         ),
-//       },
-//       {
-//         path: "/dashboard",
-//         element: (
-//           <RequireAuth>
-//             <Suspense fallback={<div>Loading...</div>}>
-//               <Dashboard />
-//             </Suspense>
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/link/:id",
-//         element: (
-//           <RequireAuth>
-//             <Suspense fallback={<div>Loading...</div>}>
-//               <Link />
-//             </Suspense>
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/:id",
-//         element: (
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <RedirectLink />
-//           </Suspense>
-//         ),
-//       },
-//     ],
-//   },
-// ]);
+const LandingPage = React.lazy(() => import('./Pages/LandingPage'));
+const RedirectLink = React.lazy(() => import('./Pages/Redirect-link'));
+const Dashboard = React.lazy(() => import('./Pages/Dashboard'));
+const Link = React.lazy(() => import('./Pages/Link'));
+const Auth = React.lazy(() => import('./Pages/auth'));
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <LandingPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/auth",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Auth />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Dashboard />
+            </Suspense>
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Link />
+            </Suspense>
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/:id",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <RedirectLink />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-   <>gfgfg</>
+    <UrlProvider>
+    <RouterProvider router={router} />
+  </UrlProvider>
   );
 }
 
